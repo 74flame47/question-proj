994daa95fd18a772ddd400c37675913e7549e910
74flame47 <hiswillbedone7777@live.com>
2019-05-07 15:21:07 -0400

    I started to create the database practice.

--- js/root.js
+++ js/root.js
@@ -37,7 +37,8 @@ console.log("You are gaining power Barry! You are building the strength to achei
 
 
 
-
+/**
+ 
 let database = [
     {
         userName: "harry4",
@@ -51,6 +52,50 @@ let newsfeed = [
         timeline: "timeline",
     }
 ];
+
+ */
+
+
+//Create a database array, along with a news feed array. 
+//Then add users into the database as objects, about 3.
+//Next add the users into the newsfeed along with their posts.
+//Create prompts that receives users information: name and password
+//I want the database to store the information that was added. So I can't use prompt. Use prompt for now then change it in the future.
+
+let database = [
+    {
+        userName: "personA",
+        password: 11,
+    },
+    {
+        userName: "personB",
+        password: 12,
+    },
+    {
+        userName: "personC",
+        password: 13,
+    },
+    {
+        userName: "personD",
+        password: 14,
+    },
+];
+
+
+let timeline = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
