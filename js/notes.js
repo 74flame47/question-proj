a671a2a271f63718249f73ed7b24972c22285c52
74flame47 <hiswillbedone7777@live.com>
2019-05-07 11:10:01 -0400

    Uploading starting files.

---
+++ js/notes.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+/*
+
+YOU CAN CREATE A FUNCTION AND ASSIGN THAT FUNCTION TO A 
+BUTTON... WHO KNEW! So I can customize the functions that
+the button carry out or redefine them. FREAKING COOL
+
+You can also get html content from a tag or redefine it using
+innerHTML = "New copy" / varible, or retreive it by simply calling it. 
+Example below.
+
+LINK:https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_innerhtml_return
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">Click the button to alert the text of this paragraph.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert(document.getElementById("demo").innerHTML);
+}
+</script>
+
+</body>
+</html> 
+ */
---
+++ js/root.js
@@ -0,0 +1,83 @@
+//alert("You're all set to run!");
+
+
+
+const btnChange = () => {
+    let choice0 = document.getElementById("choice0");
+    choice0.innerHTML = "New Answer";
+}
+
+btnChange();
+
+
+let barryB = {
+    firstName: "Barry",
+    lastName: "Burrows",
+    age: 27,
+    isLoved: true
+};
+
+let list =[
+    {
+        userName: "Harry",
+        password: "Barr",
+    }
+];
+
+
+barryB.happy = "Yes he is!";
+
+
+
+
+
+console.log("Is Barry happy? " + barryB.happy);
+console.log("You are gaining power Barry! You are building the strength to acheive your dreams! This power to do what ever you want.");
+
+
+
+
+
+let database = [
+    {
+        userName: "harry4",
+        password: "4real",
+    },
+];
+
+let newsfeed = [
+    {
+        username: "accountHolder",
+        timeline: "timeline",
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
